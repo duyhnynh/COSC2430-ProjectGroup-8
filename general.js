8a6589df35ca2733d16904d8dcbaa11b954ea23a
Ning <Thanhtudng123@gmail.com>
2024-08-20 02:50:42 +0700

    Update Header
    Update Navbar when in mobile device

--- general.js
+++ general.js
@@ -7,3 +7,16 @@ window.addEventListener("scroll", function () {
     header.className = "header";
   }
 });
+
+function showVerticalNavbar() {
+  var navbarStatus = 0;
+  var verticalNavbar = document.querySelector("#hidden-vertical-navbar");
+  if (navbarStatus == 0) {
+    verticalNavbar.style.display = "flex";
+    navbarStatus = 1;
+  }
+  if (navbarStatus == 1) {
+    verticalNavbar.style.display = "none";
+    navbarStatus = 0;
+  }
+}
