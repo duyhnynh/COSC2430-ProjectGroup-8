d477d5818c6ca70219b04054aa8483cfa37474c5
Ning <Thanhtudng123@gmail.com>
2024-08-23 23:18:06 +0700

    Seperate header and home page
    + Working header import using iframe

--- general.js
+++ general.js
@@ -1,13 +1,15 @@
-window.addEventListener("scroll", function () {
-  console.log(scrollY);
-  var header = document.querySelector("header");
-  if (window.scrollY > 0) {
-    header.className = "header-scroll";
-  } else {
-    header.className = "header";
-  }
-});
+var iframe = document.querySelector("iframe"); // Correctly target the iframe
+iframe.addEventListener("load", function () {
+  var header = iframe.contentDocument.querySelector("header"); // Access the header inside the iframe
 
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      header.className = "header-scroll";
+    } else {
+      header.className = "header";
+    }
+  });
+});
 function showVerticalNavbar() {
   var verticalNavbar = document.querySelector("#hidden-vertical-navbar");
   if (verticalNavbar.style.display == "none") {
