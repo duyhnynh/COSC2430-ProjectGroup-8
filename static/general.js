62ef522288feba2425232fa1139719275c65759c
lisa <lisa.maria.huynh1@hotmail.dk>
2024-08-30 10:20:36 +0200

    add scripts for test

---
+++ static/general.js
@@ -0,0 +1,58 @@
+var iframe = document.querySelector("iframe"); // Correctly target the iframe
+iframe.addEventListener("load", function () {
+  var header = iframe.contentDocument.querySelector("header"); // Access the header inside the iframe
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      header.className = "header-scroll";
+    } else {
+      header.className = "header";
+    }
+  });
+});
+
+window.onload = function () {
+  var navbarHeight = document.querySelector("#iframe-header");
+
+  if (!navbarHeight) {
+    console.error("#iframe-header not found in the DOM.");
+  } else {
+    console.log(navbarHeight);
+    // Proceed with your logic
+  }
+};
+
+function showVerticalNavbar() {
+  var verticalNavbar = document.querySelector("#hidden-vertical-navbar");
+  var navbarHeight = window.parent.document.querySelector("#iframe-header");
+  if (verticalNavbar.style.display == "none") {
+    verticalNavbar.style.display = "flex";
+    navbarHeight.style.height = "100%";
+  } else {
+    verticalNavbar.style.display = "none";
+    navbarHeight.style.height = "60px";
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Function to toggle instructor fields
+  function toggleInstructorFields() {
+      var instructorRadio = document.getElementById('instructor');
+      var instructorFields = document.getElementById('instructor-fields');
+      
+      if (instructorRadio.checked) {
+          instructorFields.style.display = 'block';
+      } else {
+          instructorFields.style.display = 'none';
+      }
+  }
+
+  // Add event listeners to account type radio buttons
+  var accountTypeRadios = document.querySelectorAll('input[name="account-type"]');
+  accountTypeRadios.forEach(function(radio) {
+      radio.addEventListener('change', toggleInstructorFields);
+  });
+
+  // Initial call to set the correct visibility on page load
+  toggleInstructorFields();
+});
